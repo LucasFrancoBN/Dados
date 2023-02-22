f9205d03eaea813ee168c8d9a82a7d9f3487f326
Lucas <lucasfbnavarro@gmail.com>
2023-02-21 21:56:27 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,38 +8,40 @@ const bestDayElement = document.querySelector<HTMLParagraphElement>(".bestDay");
 const tableBody =
   document.querySelector<HTMLTableSectionElement>(".table-body");
 
-const datas = await fetchDatas<Sell>();
+async function handleData() {
+  const datas = await fetchDatas<Sell>();
+  if (datas) {
+    const dataNormalizedKeys = normalizedKeys(datas);
+    if (tableBody) {
+      tableBody.innerHTML = dataNormalizedKeys
+        .map((data) => {
+          return `
+          <tr>
+            <td>${data.nome}</td>
+            <td>${data.email}</td>
+            <td>R$ ${data.moeda}</td>
+            <td>${data.forma_de_pagament}</td>
+            <td>${data.status}</td>
+          </tr>
+        `;
+        })
+        .join(" ");
+    }
+  }
 
-if (datas) {
-  const dataNormalizedKeys = normalizedKeys(datas);
-  if (tableBody) {
-    tableBody.innerHTML = dataNormalizedKeys
-      .map((data) => {
-        return `
-        <tr>
-          <td>${data.nome}</td>
-          <td>${data.email}</td>
-          <td>R$ ${data.moeda}</td>
-          <td>${data.forma_de_pagament}</td>
-          <td>${data.status}</td>
-        </tr>
-      `;
-      })
-      .join(" ");
+  if (datas) {
+    const dataNormalizedKeys = normalizedKeys(datas);
+    const statis = new Statistics(dataNormalizedKeys);
+    if (total) {
+      total.innerText += ` ${statis.total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      })}`;
+    }
+    fillList(statis.payments, "payments");
+    fillList(statis.infoPayments, "infoPayments");
+    if (bestDayElement) bestDayElement.innerText += ` ${statis.bestDay[0]}`;
+    console.log(statis.bestDay[0]);
   }
 }
-
-if (datas) {
-  const dataNormalizedKeys = normalizedKeys(datas);
-  const statis = new Statistics(dataNormalizedKeys);
-  if (total) {
-    total.innerText += ` ${statis.total.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    })}`;
-  }
-  fillList(statis.payments, "payments");
-  fillList(statis.infoPayments, "infoPayments");
-  if (bestDayElement) bestDayElement.innerText += ` ${statis.bestDay[0]}`;
-  console.log(statis.bestDay[0]);
-}
+handleData();
